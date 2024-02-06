55ee917b1fb50ffbabf5127f95613d9d31c762be
pijushghorai <pghorai098@gmail.com>
2024-02-06 21:09:45 +0530

    complete solution

---
+++ 09. Colorfull Mouse Circle/script.js
@@ -0,0 +1,26 @@
+const cursor = document.querySelector('.cursor');
+// an array of 10 colors in hex value
+const colors = [
+  '#FF6633',
+  '#FFB399',
+  '#FF33FF',
+  '#FFFF99',
+  '#00B3E6',
+  '#E6B333',
+  '#3366E6',
+  '#999966',
+  '#99FF99',
+  '#B34D4D',
+];
+// add circle to cursor and change it's color as cursor moves on the screen. Pick color from these array
+
+document.addEventListener('mousemove', function (e) {
+  cursor.style.left = e.pageX + `px`
+  cursor.style.top = e.pageY + `px`
+
+  let randomColor = colors[Math.floor(Math.random() * colors.length)]
+  cursor.style.backgroundColor = randomColor
+})
+
+
+
