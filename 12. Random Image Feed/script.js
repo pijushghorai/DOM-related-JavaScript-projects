61150a26d5313b45ecdcc2c787769caf0c3da22b
Pijush Ghorai <pghorai098@gmail.com>
2024-02-16 00:07:37 +0530

    complete

--- 12. Random Image Feed/script.js
+++ 12. Random Image Feed/script.js
@@ -1,3 +1,11 @@
-const baseURL = 'https://source.unsplash.com/all/300x300';
+const baseURL = "https://source.unsplash.com/all/300x300";
 
 // this url gives an image. Use this and NO API calls
+
+const img = document.querySelector(".content");
+const imgBtn = document.querySelector("#new-img");
+console.log(img);
+
+imgBtn.addEventListener("click", () => {
+  img.innerHTML = `<img src="${baseURL}*${Math.random()}" alt="New Img">`;
+});
